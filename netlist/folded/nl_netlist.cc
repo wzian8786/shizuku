2f6c9e9f45994ab0b36818d83bd4b01a27ccc9ca
wzian <wzian8786@gmail.com>
2023-12-11 20:30:41 +0800

    fix: fix release build crash

--- netlist/folded/nl_netlist.cc
+++ netlist/folded/nl_netlist.cc
@@ -1,7 +1,7 @@
 #include "nl_netlist.h"
 namespace netlist {
 template<uint32_t Namespace>
-Netlist<Namespace> Netlist<Namespace>::gSingleton;
+Netlist<Namespace> __attribute__((init_priority(102))) Netlist<Namespace>::gSingleton;
 
 template class Netlist<NL_DEFAULT>;
 }
